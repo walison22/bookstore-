b9f32cebce2b62293eb2eb07c184d997abb99757
walison22 <125771131+walison22@users.noreply.github.com>
2023-04-08 10:15:23 +0100

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookstoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:", r))
+}
